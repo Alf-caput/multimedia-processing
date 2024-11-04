57c8979f93e91a555f6e34636b978b754da770f2
Alf-caput <alfre.roble.aba@gmail.com>
2024-11-04 15:02:16 +0100

    Create python folder

--- Practica-1/Processing_Images_MATLAB.m
+++ Practica-1/Processing_Images_MATLAB.m
@@ -10,3 +10,11 @@ end
 % Load the image
 img = imread('DiceDataset/1/00000.bmp');
 imshow(img)
+
+disp(size(img))
+% Show the dimensions of the image
+[numRows, numCols, numColorChannels] = size(img);
+fprintf('Image size is %d rows x %d columns x %d color channels.\n', numRows, numCols, numColorChannels);
+if numColorChannels == 1
+    fprintf('The image is a single channel image (a grayscale image).\n');
+end
